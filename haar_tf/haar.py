f1b82666e3aaaa3b13cb0b37780cb5c39ac10076
Michael Eickenberg <michael.eickenberg@gmail.com>
2016-08-30 19:35:35 +0200

    added haar3_2d_conv

--- haar_tf/haar.py
+++ haar_tf/haar.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import numpy as np
+from itertools import product
+
 
 def haar1d(x, axis, concat_axis=None):
     xshape = tf.shape(x)
@@ -24,6 +26,39 @@ def haar1d(x, axis, concat_axis=None):
     return concat
 
 
+def haar3_1d(x, axis, concat_axis=None):
+    pass
+
+
+def _3x3_haar_filters():
+    diff = np.array([1., 0, -1.]).astype('float32')
+    avg = np.ones(3, dtype='float32')
+    diff_avg = [diff, avg]
+    filters = [np.outer(a, b) for a, b in
+               product(diff_avg, diff_avg)]
+    return np.array(filters).transpose(
+        1, 2, 0).reshape(3, 3, 1, 4).astype('float32')
+
+
+def haar3_2d_conv(x, strides=(2, 2), padding='SAME'):
+    xshape = x.get_shape()   # pre-evaluation shape
+    height, width, channels = [d.value for d in xshape[1:]]
+    x_batch_channel_space = tf.transpose(x, (0, 3, 1, 2))
+    new_shape = (-1, height, width, 1)
+    x_batch_times_channel_reshaped = tf.reshape(x_batch_channel_space, new_shape)
+    
+    filters = tf.constant(_3x3_haar_filters())
+    conv_strides = (1,) + strides + (1,)
+    conv_raw_ = tf.nn.conv2d(x_batch_times_channel_reshaped, filters,
+                             strides=conv_strides, padding=padding)
+    s0, s1 = strides
+    output_shape_ = (-1, channels, height // s0, width // s1, 4)
+    conv_reshaped_ = tf.reshape(conv_raw_, output_shape_)
+    conv_transposed_ = tf.transpose(conv_reshaped_, (0, 2, 3, 1, 4))
+    output_shape = (-1, height // s0, width // s1, channels * 4)
+    return tf.reshape(conv_transposed_, output_shape)
+
+    
 def haar(x, axes, concat_axis=None):
     
     if concat_axis is None:
@@ -121,3 +156,16 @@ def haar_and_1x1_relu(input_tensor, n_output_channels, scope_name,
     return output
 
 
+if __name__ == '__main__':
+
+    from skimage.data import coffee
+
+    c = (coffee() / 256.).astype('float32').reshape(1, 400, 600, 3)
+
+    x = tf.placeholder(tf.float32, shape=(1, 400, 600, 3))
+    h = haar3_2d_conv(x)
+
+    sess = tf.Session()
+    o = sess.run(h, {x: c})
+    
+    
